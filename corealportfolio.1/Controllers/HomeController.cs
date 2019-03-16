ccf368dd6c81d489fb9edbffbb440619310b40ae
losjuan95 <44878849+losjuan95@users.noreply.github.com>
2019-03-16 16:15:55 -0400

    3/14/2019
    polished up some of the project up still have some left

--- corealportfolio.1/Controllers/HomeController.cs
+++ corealportfolio.1/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using corealportfolio._1.App_Start;
+using corealportfolio._1.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,8 +15,46 @@ namespace corealportfolio._1.Controllers
     {
         public ActionResult Index()
         {
+
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> index(email model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var body = "<p>Email From:<bold>{0}({1})</p><p>Message:</p><p>{2}</p>";
+                    var from = "CoreaPortforlio<anyemailher@host.com>";
+
+                    model.Body = "this is a message from your BugTracker. The name and the email of the contacting person is above." + model.Body;
+                    var email = new MailMessage(from, ConfigurationManager.AppSettings["emailto"])
+                    {
+                        Subject = model.Subject,
+                        Body = string.Format(body, model.FromName, model.FromEmail, model.Body),
+                        IsBodyHtml = true
+                    };
+
+                    var svc = new PersonalEmail();
+                    await svc.SendAsync(email);
+
+                    return View(new email());
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    await Task.FromResult(0);
+
+                }
+            }
+            return View(model);
+
+        }
+
 
         public ActionResult About()
         {
